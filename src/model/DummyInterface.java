80fd960acb581c5adcb2586a96dbc6a3a9798175
chitakchan <chitak_chan@hotmail.com>
2021-04-21 17:37:29 +0100

    create a class WFObjWriter to allow the basic of xyz coordinate plane. Now can generate a sample rectangular plane, with triangular surface and output to wavefront .obj file format for display in blender.
    this version is now committed to a new approach to not assign x and y array, but to refer to the ulcorner, the degree latitude and longitude r and c
    so dem is ok but WFObjWriter is not, need to match the size of col and rows
    
    successfully work out scotland and hong kong.  though the resolution for hong kong is too small for its size.  
    
    now there is a feature for having an origin different from the original formed by the greenwich meridian as y axis and equator as x.
    
    now able to simply settings of parameters.  all relating to box data are now stored in box.hdr (properties file) in directory selected by user.  in turn the directory system information is stay in the munroproject.properties in the system.home directory set by windows.
    
    can print side and base plate, determine the thickness based on pct as parameter in box.hdr properties file.

---
+++ src/model/DummyInterface.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Think
+ */
+public interface DummyInterface {
+    
+    /**
+     * package model;
+
+/**
+ *
+ * @author Think
+ 
+public interface Vertice {
+    public double[] getXYZ();
+    public double[] getENZ(double x, double y);
+    public double[] getRCZ();
+}
+
+     */
+    
+}
